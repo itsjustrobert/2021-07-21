a0ec57e2628cdb97eb1827c512590bcf6a74b6a2
itsjustrobert <robertlove99999@gmail.com>
2021-07-21 18:16:40 -0500

    completed lab03

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// // define variables
+//  let x = 23; //nteger
+// let y ="23"; //string
+// let z = 20; // integer
+
+// // simple conditionals
+// if(x===y){
+// console.log("x is equal to y");
+
+
+// }
+// else{
+//     console.log("x is not equal to y");
+// }
+// // a little more advanced
+// if(z >= x){
+// console.log("z is greater than x");
+
+
+// }
+// else if(z<x){
+// console.log("z is less than x")
+// }
+// else{
+//     console.log("default message...")
+// }
+
+
+
+
+
+
+
+
+
+
---
+++ js/arrays.js
@@ -0,0 +1,44 @@
+// // basic array
+// let nameList = ["Bob", "Alfred","George","Fred"];
+// // print
+// console.log(nameList);
+// // all items have an index number beginning with 0
+
+// // print the second name in my array
+// console.log("The Second Name in the list is " + nameList[1]);
+
+// // combine with loops to process the array
+// // // for loop
+// // // for(let i = 0; i < 4; i++)
+// // for(let i = 0; i < nameList.length; i++)
+// // {
+
+// // // print the next element
+// // console.log(nameList[i]);
+// // }
+
+// // while loop that adds names to a list
+// let names=[];
+// let notDoneEnteringNames =true;
+// while(notDoneEnteringNames)
+// {
+// // enter a name each time
+// let newNAmeEntered = prompt("Enter your name or x to quit");
+// if(newNAmeEntered==="x" || newNAmeEntered==="X"){
+// notDoneEnteringNames=false;}
+// else{
+//     names.push(newNAmeEntered);
+// }
+
+// console.log("done listing names....")
+// }
+// for(let i =0; i>names.length; i++){
+   
+//     console.log(names[i]);
+
+// }
+
+
+
+// print names
+
